7ba0cc8040ced13b8c71348d7a1329d7724be819
Florian Unzicker <unzico@gmail.com>
2022-06-25 21:42:21 +0200

    feat: rewrite index file to include async fn by default

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Your code here.");
+async function main() {
+  console.log("Your code here.");
+}
+
+main()
+  .then(() => process.exit())
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
